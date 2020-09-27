b85b70cf57c7a9ca9726b1252d53685acee2a6cb
jussstinpot <jpot1234@gmail.com>
2020-09-27 15:19:43 -0700

    saving code

--- 03-Python-Challenge/PyBank/main.py
+++ 03-Python-Challenge/PyBank/main.py
@@ -1,32 +1,42 @@
 import os
 import csv
 
-csvpath = os.path.join('Resources', '.gitignore', 'budget_data.csv')
+csvpath = os.path.join("..", "..", "..", 'budget_data.csv')
+
+#Set each variable to 0
+total_months = []
+total_net = []
+average_change= []
 
 with open(csvpath) as budget:
+
     csvreader=csv.reader(budget, delimiter=',')
 
     #Skip Header for row count
     csv_header = next(csvreader)
-
-    #Total number of months 
-    months = len(list(csvreader))
-
-
-
-
-
-
-
-    print("Financial Analysis")
-    print("--------------------------------")
-    print(f"Total Months: ", str(months))
-    print(f"Total: ", "$")
-    print(f"Average Change: ", "$")
-    print(f"Greatest Increase in Profits: ")
-    print(f"Greatest Decrese in Profits: ")
-
     
+    for row in csv.reader(budget):
+      
+      # append the total months and total profit to their corresponding lists
+      total_months.append(row[0])
+      total_net.append(int(row[1]))
+    
+    for i in range(len(total_net)-1):
+        average_change.append(total_net[i+1]-total_net[i])
+
+greatest_increase = max(average_change)
+greatest_decrease = min(average_change)
+
+increase_month = average_change.index(max(average_change)) + 1
+decrease_month = average_change.index(min(average_change)) + 1
+
+print("Financial Analysis")
+print("--------------------------------")
+print(f"Total Months: {len(total_months)}")
+print(f"Total:$ {sum(total_net)}")
+print(f"Average Change:${round(sum(average_change)/len(average_change),2)}")
+print(f"Greatest Increase in Profits: {total_months[increase_month]} (${(str(greatest_increase))})")
+print(f"Greatest Decrese in Profits: {total_months[decrease_month]} (${(str(greatest_decrease))})")
 
 
 
